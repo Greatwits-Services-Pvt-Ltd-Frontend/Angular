3443b6daea2176cb24697ee15535b466b35bf0e0
unknown <hemant.verma@greatwits.com>
2021-03-17 17:47:17 +0530

    17-03-21

---
+++ src/app/app.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  YO = 'HOONIGAN';
+name= "MICHEAL KORS";
+test(){
+  return this.name;
+};
+
+obj={
+  nam :"jim morrison",
+  age : 27,
+}
+arr = ["hello","how","are","you"]
+siteurl = window.location.href;
+
+
+
+
+
+
+
+
+
+
+
+}
